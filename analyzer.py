f87dfa13236ae49a1f668bf9d5fb1cfcb887f44c
laysa.uchoa <laysa.uchoa@aiven.io>
2022-10-20 18:03:07 +0200

    Add analyzer output with formatting

--- analyzer.py
+++ analyzer.py
@@ -8,7 +8,11 @@ Run the following to check the available methods:
 
 """
 import typer
+import collections
+
 from rich.console import Console
+from rich.table import Table
+from enum import Enum
 
 from config import INDEX_NAME, client
 from helpers import log_titles
@@ -16,28 +20,46 @@ from typing import List
 
 app = typer.Typer(rich_markup_mode="rich")
 console = Console()
-resp = client.search(index=INDEX_NAME, body=query_body)
 
 
-analyzers = [
-    "standard",
-    "simple",
-    "whitespace",
-    "stop",
-    "keyword",
-    "pattern",
-    "fingerprint",
-]
+class BuiltInAnalyzer(str, Enum):
+    standard = "standard"
+    simple = "simple"
+    whitespace = "whitespace"
+    stop = "stop"
+    keyword = "keyword"
+    pattern = "pattern"
+    fingerprint = "fingerprint"
 
 
-def generate_tokens(analyzer, text):
-    res = os.indices.analyze(body={"analyzer": analyzer, "text": [text]})
+@app.command("test")
+def test_analyzer(
+    text: str,
+    analyzer: BuiltInAnalyzer = BuiltInAnalyzer.standard,
+):
+    """Tokenizer your input with a built-in analyzer of your choice"""
+    res = client.indices.analyze(body={"analyzer": analyzer, "text": [text]})
     tokens = [sample["token"] for c, sample in enumerate(res["tokens"])]
     print(f"{analyzer} \n")
     print(f"Tokens: {tokens} \n")
 
 
+@app.command("test_all")
+def test_analyzers(text: str):
+    """Check how your input is tokenized with all OpenSearch built-in analyzers"""
+    console.rule("[bold red]OpenSearch® Built-in Analyzers")
+    table = Table(title="OpenSearch® Built-in Analyzers")
+
+    table.add_column("Analyzer", justify="right", style="cyan", no_wrap=True)
+    table.add_column("Tokens", style="magenta")
+    for analyzer in BuiltInAnalyzer:
+        res = client.indices.analyze(body={"analyzer": analyzer, "text": [text]})
+        tokens = [sample["token"] for c, sample in enumerate(res["tokens"])]
+
+        table.add_row(f"{analyzer}", f"{tokens}")
+
+    console.print(table)
+
+
 if __name__ == "__main__":
-    text = "Hello my Name is Laysa.12345"
-    for analyzer in analyzers:
-        generate_tokens(analyzer, text)
+    app()
